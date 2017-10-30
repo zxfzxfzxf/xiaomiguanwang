b6bdd975d919c23ec123b521ac3e511ce2d37920
zxfzxfzxf <992895492@qq.com>
2017-10-30 15:57:24 +0800

    five

---
+++ js/move.js
@@ -0,0 +1,48 @@
+/**
+ * Created by lenovo on 2017/10/29.
+ */
+var Obl = document.getElementById('brick-promo-list');
+var aItem = Obl.getElementsByTagName('li')[0];
+//    console.log(oItem);
+aItem.onmouseover = function () {
+    aItem.zIndex = 2;
+    aItem.style.top = aItem.offsetTop - 5 + 'px';
+}
+aItem.onmouseout = function () {
+    aItem.zIndex = 1;
+    aItem.style.top = aItem.offsetTop + 5 + 'px';
+}
+
+
+var oTab_item=document.getElementsByClassName('tab-item');
+for(var i=0;i<oTab_item.length;i++){
+    oTab_item[i].index=i;
+    oTab_item[i].timer=null;
+    oTab_item[i].onmouseover=function () {
+        var oReview=this.getElementsByClassName('review-wrapper')[0];
+        show(this,80,10);
+        this.style.top = aItem.offsetTop - 5 + 'px';
+    }
+    oTab_item[i].onmouseout=function () {
+        show(this,0,-10);
+        this.style.top = aItem.offsetTop + 5 + 'px';
+    }
+
+}
+function show(obj,target,speed) {
+    var oReview=obj.getElementsByClassName('review-wrapper')[0];
+    oReview.style.opacity=1;
+    clearInterval(obj.timer);
+    obj.timer=setInterval(function () {
+        if (oReview.offsetHeight==target)
+        {
+            clearInterval(obj.timer);
+        }
+        else{
+            oReview.style.height=oReview.offsetHeight+speed+'px';
+        }
+
+
+    },20);
+
+}
